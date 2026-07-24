19bf61e5eda943230368f38a562feab1e3230423
murad <muradkv@gmail.com>
2026-07-24 22:27:54 +0300

    Initial commit

---
+++ FriendFace/App/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by murad on 24.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
