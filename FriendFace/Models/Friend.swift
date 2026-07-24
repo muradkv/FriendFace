6117963f616f4b91dc74d3baa0745f6ff86ee1ef
murad <muradkv@gmail.com>
2026-07-24 22:39:19 +0300

    Create User and Friend domain data models
    - Add Friend struct with id and name properties conforming to Codable and Identifiable
    - Add User struct containing user details, tags, and nested array of friends
    - Set up Date type for registration timestamp to support ISO-8601 decoding strategy

---
+++ FriendFace/Models/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by murad on 24.07.2026.
+//
+
+import Foundation
+
+struct Friend: Codable, Identifiable {
+    let id: UUID
+    let name: String
+}
